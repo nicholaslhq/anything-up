a82f99d9448d240d62acc1e9763d5fdd015c9650
Nicholas Lee <niick0816@gmail.com>
2025-01-24 22:12:06 +0800

    Updated NotFoundPage styles for improved text visibility and consistency

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ const NotFoundPage = () => {
     <div className="min-h-screen p-4 sm:p-10 font-[family-name:var(--font-geist-sans)]">
     <main className="flex flex-col gap-24 md:gap-32 items-center">
       <Title />
-      <div className="flex flex-col items-center text-center">
+      <div className="flex flex-col items-center text-center text-text">
           <h1 className="text-8xl md:text-9xl font-bold mb-4">404</h1>
           <p className="mb-8">The page you are looking for does not exist</p>
           <Link href="/" className="text-sm text-text underline hover:opacity-80">
