7861a0a11ac361626a19416a026a44d6dd67fd6c
Nicholas Lee <niick0816@gmail.com>
2025-02-02 20:20:19 +0800

    Refactored PostError component to remove props and standardize error message

--- src/components/post/PostError.tsx
+++ src/components/post/PostError.tsx
@@ -2,16 +2,13 @@ import React from "react";
 import { Card, CardHeader } from "@/components/ui/card";
 import PostVote from "@/components/post/PostVote";
 
-interface PostErrorProps {
-	message: string;
-}
 
-const PostError: React.FC<PostErrorProps> = ({ message }) => {
+const PostError: React.FC = () => {
 	return (
 		<div className="flex gap-3 md:gap-5 w-full sm:max-w-lg">
 			<Card className="flex-1 bg-red-300 dark:bg-red-800">
 				<CardHeader>
-					<p>Error: {message}</p>
+					<p>Oops! Something went wrong</p>
 				</CardHeader>
 			</Card>
 			<PostVote
