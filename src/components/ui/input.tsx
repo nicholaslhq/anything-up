769580eda5767a67e364f0082f7d0d6e2e4a8442
Nicholas Lee <niick0816@gmail.com>
2025-04-23 19:34:11 +0800

    Refactored component files for consistency in import statements and formatting

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,34 +1,34 @@
-import * as React from 'react'
-import { cn } from '@/lib/utils'
+import * as React from "react";
+import { cn } from "@/lib/utils";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
-  prefix?: string;
+	extends React.InputHTMLAttributes<HTMLInputElement> {
+	prefix?: string;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, prefix, ...props }, ref) => {
-    return (
-      <div className="relative">
-        {prefix && (
-          <div className="absolute left-3 top-1/2 transform -translate-y-1/2 text-sm text-text">
-            {prefix}
-          </div>
-        )}
-        <input
-          type={type}
-          className={cn(
-            'flex h-10 w-full rounded-base border-2 text-text shadow-shadow focus:shadow-none font-base selection:bg-main selection:text-mtext border-border bg-bw px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-base focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-            prefix ? 'pl-6' : '',
-            className,
-          )}
-          ref={ref}
-          {...props}
-        />
-      </div>
-    )
-  },
-)
-Input.displayName = 'Input'
+	({ className, type, prefix, ...props }, ref) => {
+		return (
+			<div className="relative">
+				{prefix && (
+					<div className="absolute left-3 top-1/2 transform -translate-y-1/2 text-sm text-text">
+						{prefix}
+					</div>
+				)}
+				<input
+					type={type}
+					className={cn(
+						"flex h-10 w-full rounded-base border-2 text-text shadow-shadow focus:shadow-none font-base selection:bg-main selection:text-mtext border-border bg-bw px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-base focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+						prefix ? "pl-6" : "",
+						className
+					)}
+					ref={ref}
+					{...props}
+				/>
+			</div>
+		);
+	}
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
